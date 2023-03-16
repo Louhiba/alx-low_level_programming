ce3d06933084af9a7194e22f8294e86be74271c6
Louhiba <hibaloundi@gmail.com>
2023-03-16 16:53:15 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,11 +9,11 @@ float e;
 long int c;
 char a;
 long long int d;
-printf("Size of int: %i byte(s)\n", sizeof(a));
-printf("Size of float: %i byte(s)\n", sizeof(b));
-printf("Size of double: %i byte(s)\n", sizeof(c));
-printf("Size of char: %i byte(s)\n", sizeof(d));
-printf("Size of char: %zu byte(s)\n", sizeof(e));
+printf("Size of char: %i byte(s)\n", sizeof(a));
+printf("Size of int: %i byte(s)\n", sizeof(b));
+printf("Size of long int: %i byte(s)\n", sizeof(c));
+printf("Size of long long int: %i byte(s)\n", sizeof(d));
+printf("Size of float: %i byte(s)\n", sizeof(e));
 return (0);
 }
 
