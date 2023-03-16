410ff019d89923031ab1fab266693dfcd7652990
Louhiba <hibaloundi@gmail.com>
2023-03-16 15:30:44 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+* main - a program that print a ligne using printf
+* Return: 0 (succses)
+*/
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
