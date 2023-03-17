df4cabe8c148f92ed54491f61c2f1a209a90cf53
Louhiba <hibaloundi@gmail.com>
2023-03-17 12:39:23 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry number
+ *Return: 0 (succses)
+*/
+int main(void)
+{int x;
+for (x = 0 ; x < 10 ; x++)
+{
+printf("%d", x);
+}
+printf("\n");
+return (0);
+}
+
