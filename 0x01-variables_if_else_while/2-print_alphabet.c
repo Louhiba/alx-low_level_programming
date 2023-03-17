b0c533fbc28a95ea9de8c11063ea5bd301484007
Louhiba <hibaloundi@gmail.com>
2023-03-17 11:45:08 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - ENTRY point
+ * Return: 0 (succses)
+*/
+int main(void)
+{char s;
+for (s = 'a'; s <= 'z'; s++)
+{
+putchar(s);
+}
+putchar('\n');
+return (0);
+}
+
