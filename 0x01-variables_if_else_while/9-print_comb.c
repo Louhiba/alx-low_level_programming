91544f0d539525f703ace6d452eb77a6bc87e00c
Louhiba <hibaloundi@gmail.com>
2023-03-18 16:11:39 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,9 +7,10 @@ int main(void)
 {int i;
 for (i = 0 ; i < 10 ; i++)
 {       putchar(i + '0');
-	if (i == 9) continue;
-	putchar(',');
-	putchar(' ');
+if (i == 9)
+continue;
+putchar(',');
+putchar(' ');
 }
 putchar('\n');
 return (0);
