40762ee8bbe4855b02e3e0574195155c15dae929
Louhiba <hibaloundi@gmail.com>
2023-03-21 16:40:34 -0700

    doneee 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,12 +7,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		_putchar(43);
 		return (1);
+	}
 	else if (n == 0)
+	{
 		return (45);
 		_putchar('0');
+	}
 	else
+	{
 	       _putchar(45);
 		return (-1);
+	}
 }
