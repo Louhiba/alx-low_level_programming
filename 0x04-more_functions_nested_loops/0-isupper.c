c2fff6786fb9b9e4d1bd9161bc5482bcab9d32e2
Louhiba <hibaloundi@gmail.com>
2023-03-23 16:26:52 -0700

    do it 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - Entry upper character
+ * @c: checker character
+ * Return: 0 or 1
+*/
+int _isupper(int c)
+{
+
+if (c >= 97 && c <= 122)
+{
+return (0);
+}
+else
+return (1);
+}
