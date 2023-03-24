53168fa9f8f308b8bf7bdb4a591bc04585fdf9e9
Louhiba <hibaloundi@gmail.com>
2023-03-24 13:47:54 -0700

    tasck 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,5 +6,19 @@
  */
 void print_number(int n)
 {
-_putchar('0' + n);
-}
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
