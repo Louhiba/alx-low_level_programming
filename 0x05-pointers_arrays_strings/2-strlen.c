299809be7300146c46ec0de7c8371eab987001c0
Louhiba <hibaloundi@gmail.com>
2023-03-27 09:33:17 -0700

    done 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - Output the lenght of string;
+ * @s: string to be checker
+ * Return: the lenght of string
+*/
+int _strlen(char *s)
+{
+	int a;
+
+	a = strlen(s);
+	return (a);
+}
