b3401fabe07aa5418003bb9f0e43dd1c42ab3d59
Louhiba <hibaloundi@gmail.com>
2023-04-03 16:28:22 -0700

    done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - print character  a number of time
+ * @s: character to be checker
+ * @b: seconde character to be checker
+ * @n: number of time
+ * Return: character
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (&s[i]);
+}
