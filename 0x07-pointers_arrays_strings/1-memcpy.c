541189a01b16838136ec8f6166d9d3b9857d18b2
Louhiba <hibaloundi@gmail.com>
2023-04-03 16:39:03 -0700

    done

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * _memcpy - copy string in another string 
+ * _memcpy - copy string in another string
  * @dest: string to be copy in it
  * @src: copy string
  * @n: number of character
+ * Return: string
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -11,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src [i];
+		dest[i] = src[i];
 	}
 	return (&dest[i]);
 }
