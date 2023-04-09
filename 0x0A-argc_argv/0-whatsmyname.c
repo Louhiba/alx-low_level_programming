a2aa93015e74fb4cdd5064ef41a8d0b22e5d95d1
Louhiba <hibaloundi@gmail.com>
2023-04-09 10:03:54 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * main - put string
+ * @argc: size of string
+ * @argv: string 
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
