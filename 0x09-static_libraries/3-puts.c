16d712d024c106ee495db5071cd925cc5e368d9a
Louhiba <hibaloundi@gmail.com>
2023-04-10 14:09:04 -0700

    done

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,9 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 /**
  * _puts - put character
  * @s: character
 */
 void _puts(char *s)
 {
-_putchar(*s);
+	_putchar(*s);
 }
