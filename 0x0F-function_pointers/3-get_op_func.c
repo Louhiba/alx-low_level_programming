df9c68244e284d0a282a95dad56f5c23cdffbaa6
Louhiba <hibaloundi@gmail.com>
2023-04-20 09:41:52 -0700

    all done

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,10 +17,10 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-    };
-    int i = 0;
-    
-    while (ops[i].op != NULL && *(ops[i].op) != *s)
+	};
+	int i = 0;
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-    return (ops[i].f);
+	return (ops[i].f);
 }
