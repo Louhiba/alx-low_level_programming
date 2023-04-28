18078073dda9baae8959a7513f9cb73f767d8c61
Louhiba <hibaloundi@gmail.com>
2023-04-28 14:13:24 -0700

    mut

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,10 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
  * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
+ * @str: string to be evaluated
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *str)
@@ -21,8 +19,7 @@ int is_digit(char *str)
 }
 /**
  * _strlen - returns the length of a string
- * @s: string
- *
+ * @str: string
  * Return: the length of the string
  */
 int _strlen(char *str)
@@ -39,16 +36,15 @@ int _strlen(char *str)
  * main - multiplies two positive numbers
  * @argc: number
  * @argv: array
- *
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	char *n1, *n2, str[5] = "Error";
-	int *r, l1, l2, l, c, d1, d2, n, i = 0;
+	int *r, l1, l2, l, c = 0, d1, d2, n, i = 0;
 
 	n1 = argv[1], n2 = argv[2];
-	if(argc != 3 || !is_digit(n1) || !is_digit(n2))
+	if (argc != 3 || !is_digit(n1) || !is_digit(n2))
 	{
 		_putchar(str[5]);
 		_putchar('\n');
@@ -58,55 +54,30 @@ int main(int argc, char *argv[])
 	l2 = _strlen(n2);
 	l = l1 + l2 + 1;
 	r = malloc(sizeof(int) * l);
-	for (n= 0; n <= l1 + l2; n++)
+	for (n = 0; n <= l1 + l2; n++)
 		r[i] = 0;
 	for (l1 = l1 - 1; l1 >= 0; l1--)
-
 	{
-
 		d1 = n1[l1] - '0';
-
-		c = 0;
-
 		for (l2 = _strlen(n2) - 1; l2 >= 0; l2--)
-
 		{
-
 			d2 = n2[l2] - '0';
-
 			c += r[l1 + l2 + 1] + (d1 * d2);
 			r[l1 + l2 + 1] = c % 10;
-
 			c /= 10;
-
 		}
-
 		if (c > 0)
-
 			r[l1 + l2 + 1] += c;
-
 	}
-
 	for (n = 0; n < l - 1; n++)
-
 	{
-
 		if (r[n])
-
 			i = 1;
-
-		if (i)
-
-			_putchar(r[n] + '0');
-
+		_putchar(r[n] + '0');
 	}
-
 	if (!i)
 		_putchar('0');
-
 	_putchar('\n');
-
 	free(r);
-
 	return (0);
 }
