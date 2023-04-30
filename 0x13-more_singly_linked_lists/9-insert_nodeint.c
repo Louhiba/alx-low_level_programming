026c448b804747462929397470866266dc1b4b6a
Louhiba <hibaloundi@gmail.com>
2023-04-30 16:11:28 -0700

    tir

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *p = *head;
 	listint_t *an;
-	unsigned int i = 0;
+	unsigned int i;
 
 	an = malloc(sizeof(listint_t));
 	if (!an || !p)
@@ -35,6 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 		else
 			p = p->next;
+	
 	}
 	return (NULL);
 }
